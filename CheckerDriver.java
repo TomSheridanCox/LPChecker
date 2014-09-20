f802f17362fd74f59cb1662a1b6c74c6b2ac4c4e
TomSheridanCox <tom.sheridan.cox@gmail.com>
2014-09-20 19:41:29 +0100

    Revert "Initial Commit"
    This reverts commit bf21b341f1564abbf152e61a31419132993978d3.

--- CheckerDriver.java
+++
@@ -1,8 +0,0 @@
-
-public class CheckerDriver {
-
-	public static void main(String[] args) {
-		UIHandler uI = new UIHandler();
-	}
-
-}
--- QueryHandler.java
+++
@@ -1,7 +0,0 @@
-
-public class QueryHandler 
-{
-	public QueryHandler(){
-		
-	}
-}
